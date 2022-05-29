8cfbb1ab983a95d45cce0e5ec3b419d5b4b0e2ca
Belkacem Berchiche <b_berchiche@esi.dz>
2022-05-29 19:27:52 +0200

    Refactor and integrate firebase

--- screens/habits/EditHabit.tsx
+++ screens/habits/EditHabit.tsx
@@ -33,10 +33,10 @@ export const EditHabit = ({ navigation, route }) => {
             />
 
             <Text style={{ fontSize: 20 }}>How beneficial is this activity to you?</Text>
-            <MultiSlider values={[habit.benefits? habit.benefits: 0]} min={0} max={3} onValuesChange={(values) => habit.benefits = values[0]} />
+            <MultiSlider values={[habit.benefits?? 0]} min={0} max={3} onValuesChange={(values) => habit.benefits = values[0]} />
 
             <Text style={{ fontSize: 20 }}>How fun is this activity to you?</Text>
-            <MultiSlider values={[habit.fun? habit.fun: 0]} min={0} max={3} onValuesChange={(values) => habit.fun = values[0]} />
+            <MultiSlider values={[habit.fun?? 0]} min={0} max={3} onValuesChange={(values) => habit.fun = values[0]} />
 
             {/* TODO Style the buttons correctly */}
             <View style={{flexDirection: 'row'}}>
