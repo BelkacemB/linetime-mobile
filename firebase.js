7056abffd963a5e6c9beb150ceb48d657ae45c14
Belkacem Berchiche <b_berchiche@esi.dz>
2022-05-29 20:08:13 +0200

    made firebase work

---
+++ firebase.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+import {getDatabase, ref, onValue} from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDA_cS264fnfaxMJvSaCoeC2-odkm57BBw",
+  authDomain: "linetime-c6e02.firebaseapp.com",
+  databaseURL: "https://linetime-c6e02-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "linetime-c6e02",
+  storageBucket: "linetime-c6e02.appspot.com",
+  messagingSenderId: "1075377844207",
+  appId: "1:1075377844207:web:b8327bcaa6dea2e8afd01a"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getDatabase(app);
+
+// get the users collection
+export const usersRef = ref(db, "users");
+// print the users collection
+onValue(usersRef, (snapshot) => {
+    console.log(snapshot.val());
+    }
+);
